c0591b62e842869d33a44691a8d3c3094f50ed42
FaridKusuma <58212917+MR-MFKA399@users.noreply.github.com>
2023-11-11 15:34:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const body = document.querySelector("body"),
+      modeToggle = body.querySelector(".mode-toggle");
+      sidebar = body.querySelector("nav");
+      sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode ==="dark"){
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if(getStatus && getStatus ==="close"){
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")){
+        localStorage.setItem("mode", "dark");
+    }else{
+        localStorage.setItem("mode", "light");
+    }
+});
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if(sidebar.classList.contains("close")){
+        localStorage.setItem("status", "close");
+    }else{
+        localStorage.setItem("status", "open");
+    }
+})
+
+const menuLinks = document.querySelectorAll(".nav-links li a");
+const menuContents = document.querySelectorAll(".menu-content");
+
+menuLinks.forEach((link, index) => {
+    link.addEventListener("click", () => {
+        menuContents.forEach((content) => {
+            content.classList.remove("active");
+        });
+
+        menuContents[index].classList.add("active");
+    });
+});
